59858753265f1e922b21c9c619da689902f9a41f
Unknown <neto3333@gmail.com>
2020-01-24 18:55:21 -0400

    Criado projeto.

---
+++ Teste_James/Teste_James/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teste_James
+//
+//  Created by Ruy de Ascencão Neto on 24/01/20.
+//  Copyright © 2020 Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
